35827202e15653538129e541bd94f9565569dec0
Chezacar <chezacar@163.com>
2020-05-27 20:42:37 +0800

    lab4

---
+++ lab4/Mycode/Mainactivity.java
@@ -0,0 +1,17 @@
+package edu.sjtu.zhusy54.qrcode;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        
+    }
